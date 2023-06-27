31df984965729c690d7ea0c62a1812d4f86ad61a
Vaibhav <vaibhavbaheti28@gmail.com>
2023-06-27 18:17:41 +0530

    contact and about pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,12 @@
-
 import React, { useState, useContext } from "react";
-import { links as Links, nav_ul, nav, centre, span, left } from "../src/screens/homepage/styles";
+import {
+  links as Links,
+  nav_ul,
+  nav,
+  centre,
+  span,
+  left,
+} from "../src/screens/homepage/styles";
 
 import { useRouter } from "next/router";
 import { myContext } from "@/src/modules/App-context";
@@ -8,8 +14,7 @@ import HomePage from "@/src/screens/homepage";
 import Terminal from "@/src/modules/terminal";
 import { Header } from "@/src/modules/Components/header";
 const About = () => {
-
-  const {tabs} =useContext(myContext);
+  const { tabs } = useContext(myContext);
   const router = useRouter();
   const onClickTab = (val: string) => {
     console.log(val);
@@ -21,29 +26,21 @@ const About = () => {
     <Terminal>
       <Links>
         <Header />
-        <div style={{ alignSelf: "center"}} >
+        <div style={{ alignSelf: "center" }}>
           <h1 style={{ color: "red" }}>About Page</h1>
         </div>
-          <div className={nav}>
-            <div className={nav_ul}>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("about")}>
-                  <span className={`${span}`}>$ About Me</span>
-                </div>
-              </div>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("contact")}>
-                  <span className={`${span}`}>$ Contact</span>
-                </div>
-              </div>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("projects")}>
-                  <span className={`${span}`}>$ Projects</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
+        <div style={{fontFamily: "Arial , san-serif", textAlign: "center"}}>
+          <h1 style={{fontSize: "28px", fontWeight: "700"}}>Welcome to my portfolio!</h1>
+          <p style={{fontSize: "16px", lineHeight: "1.5", margin: "20px auto", maxWidth: "600px"}}>
+            &emsp; With a passion for web development and extensive experience, I
+            specialize in crafting visually appealing and intuitive websites. I
+            possess expertise in front-end and back-end development, utilizing
+            the latest technologies and industry best practices. From clean code
+            and responsive layouts to seamless user experiences, I ensure that
+            every project exceeds expectations. Let&apos;s collaborate and bring your
+            ideas to life with an amazing web presence.
+          </p>
+        </div>
       </Links>
     </Terminal>
   );
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,12 @@
-
 import React, { useState, useContext } from "react";
-import { links as Links, nav_ul, nav, centre, span, left } from "../src/screens/homepage/styles";
+import {
+  links as Links,
+  nav_ul,
+  nav,
+  centre,
+  span,
+  left,
+} from "../src/screens/homepage/styles";
 
 import { useRouter } from "next/router";
 import { myContext } from "@/src/modules/App-context";
@@ -8,8 +14,7 @@ import HomePage from "@/src/screens/homepage";
 import Terminal from "@/src/modules/terminal";
 import { Header } from "@/src/modules/Components/header";
 const Contact = () => {
-
-  const {tabs} =useContext(myContext);
+  const { tabs } = useContext(myContext);
   const router = useRouter();
   const onClickTab = (val: string) => {
     console.log(val);
@@ -21,29 +26,25 @@ const Contact = () => {
     <Terminal>
       <Links>
         <Header />
-        <div style={{alignSelf: "center"}}>
+        <div style={{ alignSelf: "center" }}>
           <h1 style={{ color: "red" }}>Contact Page</h1>
         </div>
-          <div className={nav}>
-            <div className={nav_ul}>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("about")}>
-                  <span className={`${span}`}>$ About Me</span>
-                </div>
-              </div>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("contact")}>
-                  <span className={`${span}`}>$ Contact</span>
-                </div>
-              </div>
-              <div className={`${left}`}>
-                <div onClick={() => onClickTab("projects")}>
-                  <span className={`${span}`}>$ Projects</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
+        <div style={{textAlign: "center", margin: "20px auto", maxWidth: "400px"}}>
+          <h2 style={{fontSize: "24px", fontWeight: "700", marginBottom: "10px"}}>Contact Me</h2>
+          <p style={{fontSize: "16px", marginBottom: "20px"}}>
+            I&apos;m thrilled to connect with you! Whether you have a question, an
+            exciting project idea, or just want to say hi, feel free to reach
+            out using the form below or through the provided contact details.
+            I&apos;ll get back to you as soon as possible. Looking forward to hearing
+            from you!
+          </p>
+          <form style={{width: "100%", padding: "10px", marginBottom: "10px"}}>
+            <input type="text" placeholder="Your Name" style={{width: "100%", padding: "10px", marginBottom: "10px"}}/>
+            <input type="email" placeholder="Your Email" style={{width: "100%", padding: "10px", marginBottom: "10px"}}/>
+            <textarea placeholder="Your Message" style={{width: "100%", padding: "10px", marginBottom: "10px", height: "120px"}}></textarea>
+            <button type="submit" style={{backgroundColor: "#007bff", color: "#fff", border: "none",padding: "10px 20px",cursor: "pointer"}}>Send Message</button>
+          </form>
+        </div>
       </Links>
     </Terminal>
   );
