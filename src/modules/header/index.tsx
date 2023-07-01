f171794ab4853d5c4d38c04d6cca40ee37bf52b7
Vaibhav <vaibhavbaheti28@gmail.com>
2023-07-01 13:05:03 +0530

    Click fix

--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { activeTab, tab as tabStyles, header } from "./styles";
 import { useRouter } from "next/router";
 import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
@@ -7,53 +7,39 @@ import { tableRowClasses } from "@mui/material";
 
 export const Header = () => {
   const { tabs, setTabs, setSelectedTab, selectedTab } = useContext(myContext);
-  const [localtabs, setLocalTabs] = useState(tabs);
-  const [localSelectedTab, setLocalSelectedTab] = useState(selectedTab);
   const { push } = useRouter();
   const chooseTab = (val: number) => {
-    localSelectedTab !== undefined && setLocalSelectedTab(val);
+    selectedTab !== undefined && setSelectedTab(val);
   };
   const onClose = (val: number) => {
     if (val === 0) return;
-    if (val === localSelectedTab) {
-      setLocalSelectedTab((prev) => prev - 1);
-      console.log(val);
-      console.log(selectedTab);
+    if (val === selectedTab) {
+      setSelectedTab(val - 1);
     }
 
-    console.log(localSelectedTab);
-
-    setLocalTabs(
-      localtabs.filter((_tab, idx) => {
+    console.log(selectedTab);
+    setTabs(
+      tabs.filter((_tab, idx) => {
         return idx !== val;
       })
     );
   };
 
   useEffect(() => {
-    console.log(localtabs);
-    console.log(localSelectedTab);
-    setTabs(localtabs);
-  }, [localtabs]);
-  useEffect(() => {
-    setSelectedTab(localSelectedTab);
-  }, [localSelectedTab]);
-  useEffect(() => {
-    console.log(selectedTab, "streig");
-    if (localSelectedTab === 0) push(`/`);
+    if (selectedTab === 0) push(`/`);
     else {
       console.log(tableRowClasses);
-      localSelectedTab && push(`/${localtabs[localSelectedTab]}`);
+      selectedTab && push(`/${tabs[selectedTab]}`);
     }
-  }, [localSelectedTab]);
+  }, [selectedTab]);
 
   return (
     <div className={header}>
       {tabs.map((tab: string, idx: number) => {
         return idx === selectedTab ? (
-          <div key={tab} className={activeTab} onClick={() => chooseTab(idx)}>
+          <div key={tab} className={activeTab}>
             <div style={{ display: "flex" }}>
-              <p> {tab} </p>
+              <p onClick={() => chooseTab(idx)}> {tab} </p>
               <CancelRoundedIcon
                 fontSize="small"
                 onClick={() => onClose(idx)}
@@ -61,9 +47,9 @@ export const Header = () => {
             </div>
           </div>
         ) : (
-          <div key={tab} className={tabStyles} onClick={() => chooseTab(idx)}>
+          <div key={tab} className={tabStyles}>
             <div style={{ display: "flex" }}>
-              <p> {tab} </p>
+              <p onClick={() => chooseTab(idx)}> {tab} </p>
               <CancelRoundedIcon
                 fontSize="small"
                 onClick={() => onClose(idx)}
