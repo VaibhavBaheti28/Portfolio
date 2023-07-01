ba17ef86b1e5c86041e629dcf75c879402fb2db9
Vaibhav <vaibhavbaheti28@gmail.com>
2023-07-01 13:08:37 +0530

    no locall

--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -9,38 +9,26 @@ export const Header = () => {
   const { tabs, setTabs, setSelectedTab, selectedTab } = useContext(myContext);
   const { push } = useRouter();
   const chooseTab = (val: number) => {
-    localSelectedTab !== undefined && setLocalSelectedTab(val);
+    selectedTab !== undefined && setSelectedTab(val);
   };
   const onClose = (val: number) => {
     if (val === 0) return;
-    if (val === localSelectedTab) {
-      setLocalSelectedTab((prev) => prev - 1);
-      console.log(val);
-      console.log(selectedTab);
+    if (val === selectedTab) {
+      setSelectedTab(val - 1);
     }
 
-    console.log(localSelectedTab);
-
-    setLocalTabs(
-      localtabs.filter((_tab, idx) => {
+    console.log(selectedTab);
+    setTabs(
+      tabs.filter((_tab, idx) => {
         return idx !== val;
       })
     );
   };
   useEffect(() => {
-    console.log(localtabs);
-    console.log(localSelectedTab);
-    setTabs(localtabs);
-  }, [localtabs]);
-  useEffect(() => {
-    setSelectedTab(localSelectedTab);
-  }, [localSelectedTab]);
-  useEffect(() => {
-    console.log(selectedTab, "streig");
-    if (localSelectedTab === 0) push(`/`);
+    if (selectedTab === 0) push(`/`);
     else {
       console.log(tableRowClasses);
-      localSelectedTab && push(`/${localtabs[localSelectedTab]}`);
+      selectedTab && push(`/${tabs[selectedTab]}`);
     }
   }, [selectedTab]);
 
@@ -48,7 +36,7 @@ export const Header = () => {
     <div className={header}>
       {tabs.map((tab: string, idx: number) => {
         return idx === selectedTab ? (
-          <div key={tab} className={activeTab} onClick={() => chooseTab(idx)}>
+          <div key={tab} className={activeTab}>
             <div style={{ display: "flex" }}>
               <p onClick={() => chooseTab(idx)}> {tab} </p>
               <CancelRoundedIcon
